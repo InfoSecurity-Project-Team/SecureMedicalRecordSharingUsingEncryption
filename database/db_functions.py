f56af2ec3edd5b6cc07e25763a2d07b3259f5c55
Talha Aamir <talhaaamir4990@gmail.com>
2025-05-04 17:12:57 +0500

    function for authentication

---
+++ database/db_functions.py
@@ -0,0 +1,15 @@
+from db_connection import get_connection
+
+def authenticate_user(username, password, user_type):
+    table = "doctors" if user_type.lower() == "doctor" else "patients"
+    query = f"SELECT * FROM {table} WHERE name=%s AND password=%s"
+
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(query, (username, password))
+    result = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    return result
