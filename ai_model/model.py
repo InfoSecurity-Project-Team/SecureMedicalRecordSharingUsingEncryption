a51ddcbc4a5446e2252b0aee0f0018c40927d9bf
Haris Ali <malikharis2003@gmail.com>
2025-05-05 14:06:38 +0500

    Add preprocessing for new data

--- ai_model/model.py
+++ ai_model/model.py
@@ -1,3 +1,15 @@
+import pandas as pd
+
+def preprocess(dataframe : pd.DataFrame) -> pd.DataFrame:
+    boolean_columns = ['Fever', 'Cough', 'Fatigue', 'Difficulty Breathing']
+    categorical_columns = ['Blood Pressure', 'Cholesterol Level']
+    for col in boolean_columns:
+        dataframe[col] = dataframe[col].replace({'Yes': True, 'No': False})
+    for col in categorical_columns:
+        dataframe[col] = dataframe[col].replace({'Low': 0, 'Normal': 1, 'High': 2})
+    dataframe['Gender'] = dataframe['Gender'].replace({'Male': 1, 'Female': 0})
+    return dataframe
+
 def diagnose(data: dict):
     pass
 
